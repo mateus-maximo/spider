9697c2d5c40457bba18f0eb7af77d1ede87aca98
Mateus Souza <mateushen19@outlook.com>
2026-08-11 15:42:09 -0300

    feat(spider): return typed callback result

--- chapter-3/spider-callback/spider.ts
+++ chapter-3/spider-callback/spider.ts
@@ -2,31 +2,48 @@ import path from 'node:path';
 import fs from 'node:fs';
 import superagent from 'superagent';
 
+type ResultCallback = (err: Error | null, filename: string, downloaded: boolean) => void;
+
 function main() {
   try {
     const url = new URL(process.argv[2] ?? 'http://localhost:8080')
-    const filename = urlToFilename(url)
-
-    fs.access(filename, (err) => {
-      if (!err) {
-        console.log(`${filename} was already downloaded`)
-        return
-      }
-
-      if (err.code === 'ENOENT') {
-        download(url, filename);
-        return
-      }
-
-      console.error(err)
-      process.exitCode = 1
-    })
-
+    spider(url, resultCallback)
   } catch (e) {
     console.error('Invalid URL');
     process.exitCode = 1;
   }
+}
 
+function resultCallback(err: Error | null, filename: string, downloaded: boolean) {
+  if (err) {
+    console.error(err)
+    process.exitCode = 1
+    return
+  }
+
+  if (downloaded) {
+    console.log(`Downloaded ${filename}`)
+  } else {
+    console.log(`${filename} was already downloaded`)
+  }
+}
+
+function spider(url: URL, callback: ResultCallback) {
+  const filename = urlToFilename(url)
+
+  fs.access(filename, (err) => {
+    if (!err) {
+      callback(null, filename, false)
+      return
+    }
+
+    if (err.code === 'ENOENT') {
+      download(url, filename, callback);
+      return
+    }
+
+    callback(err, filename, false)
+  })
 }
 
 function urlToFilename(url: URL): string {
@@ -43,31 +60,31 @@ function urlToFilename(url: URL): string {
   return path.join('downloads', siteDirectory, filename);
 }
 
-function download(url: URL, filename: string) {
+function download(url: URL, filename: string, callback: ResultCallback) {
   console.log(`Downloading ${url} to ${filename}`)
   superagent.get(url.href).accept('html').end((error, response) => {
     if (error) {
-      console.error(error)
+      callback(error, filename, false)
       return
     }
 
     fs.mkdir(path.dirname(filename), { recursive: true }, (err) => {
       if (err) {
-        console.error(err)
+        callback(err, filename, false)
         return
       }
 
       if (response.type != 'text/html') {
-        console.log(`Skipping ${url} (not an HTML page)`);
+        callback(new Error(`Expected text/html but got ${response.type}`), filename, false)
         return
       }
 
       fs.writeFile(filename, response.text, (err) => {
         if (err) {
-          console.error(err)
+          callback(err, filename, false)
           return
         }
-        console.log(`Downloaded ${url} to ${filename}`)
+        callback(null, filename, true)
       })
     })
   })
